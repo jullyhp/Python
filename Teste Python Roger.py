6a7fc9770a7ba5efda8353e4a3863b416e8b969e
jullyhp <95542784+jullyhp@users.noreply.github.com>
2021-12-04 17:25:35 -0300

    Add files via upload

---
+++ Teste Python Roger.py
@@ -0,0 +1,29 @@
+A=10
+B=20
+SOMA= A+B
+print(SOMA)
+print("---")
+
+
+Divisível= (A+B)/2
+print(Divisível)
+print("divisão ")
+string = " divisão "      
+print(string, Divisível)
+print(f"variável {A}+{B}/2= {Divisível}")
+
+a = 10
+b = 20
+soma = a + b
+print(soma)
+
+print("---")
+
+dividir = soma/2
+print (dividir)
+print (soma/2)
+print ("Divisão ")
+string = "Divisão "
+print (string, dividir)
+print (f"variavel {a} + {b} / 2 = {dividir}")
+print (f"variavel {a} + {b} / 2 = {dividir}")
