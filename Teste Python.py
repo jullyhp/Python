05a0cc5ecaa00a8f881d8a880d8053b328a88458
jullyhp <95542784+jullyhp@users.noreply.github.com>
2021-12-04 17:20:39 -0300

    Add files via upload

---
+++ Teste Python.py
@@ -0,0 +1,13 @@
+A=10
+B=20
+SOMA= A+B
+print(SOMA)
+print("---")
+
+
+Divisível= (A+B)/2
+print(Divisível)
+print("divisão ")
+string = " divisão "      
+print(string, Divisível)
+print(f"variável {A}+{B}/2= {Divisível}")
